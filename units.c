bfc5f60af7c48ce3b35d80a7e2444bab136cd4a3
Michael <mmwema403@gmail.com>
2026-09-15 22:57:54 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,25 @@
+/*
+(Multiline comment)
+Author: Joe Michael
+Reg number: BCS-05-0209/2026
+Description: Academic units offered this semester display program
+Date: 12/09/2026
+Version 1
+*/
+
+//pre-processor directive
+#include <stdio.h> // printf()
+
+int main(){
+//Display title
+printf("Units Offered This Semester:\n");
+//Display units
+printf("1. Introduction to Programming\n");
+printf("2. Data structures\n");	
+printf("3. Computer Networks\n");
+printf("4. Database Systems\n");
+printf("5. Software Engineering\n");
+
+
+return 0; //execution successfull
+}
